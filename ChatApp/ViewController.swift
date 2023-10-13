9e317a8fd9d4014a9dc79d6a139733020f57003d
Hitesh Moudgil <200378502@student.georgianc.on.ca>
2023-10-13 13:03:33 -0400

    project created

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Hitesh Moudgil on 2023-10-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
